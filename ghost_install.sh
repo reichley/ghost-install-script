1e398fc32a85cfede05d442e67e28fc1fa318788
reichley <nick@reichley.co>
2016-12-10 23:28:10 -0500

    Update ghost_install.sh
    Moved "-qq" to make update/upgrade less noisy

--- ghost_install.sh
+++ ghost_install.sh
@@ -26,7 +26,7 @@ apt-get -qq update
 apt-get -qq dist-upgrade
 echo 
 echo "installing node.js and npm"
-apt-get install -qq unzip wget nodejs-legacy npm
+apt-get -qq install unzip wget nodejs-legacy npm
 mkdir -p /var/www/; cd /var/www/; wget https://ghost.org/zip/ghost-latest.zip
 sleep 1
 unzip -d ghost ghost-latest.zip; cd ghost/
@@ -34,7 +34,7 @@ npm install --production
 cp config.example.js config.js  
 echo 
 echo "installing nginx..."
-apt-get install -qq nginx
+apt-get -qq install nginx
 echo
 echo "editing nginx default site and ghost config to allow ghost to proxy through port 80"
 echo
